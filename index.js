10bed6929c37853cb54e66f25c71b31f9bc4bab3
Zac Booth <Zac.Booth01@gmail.com>
2021-10-13 13:08:35 +0100

    Added words to lits

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function selectRandomMessage(){
-    let firstWord = []
-    let secondWord = []
-    let thirdWord = []
-    let fourthWord = []
-    let fifthWord = []
+    let firstWord = ["a", "the", "one", "two", "many"]
+    let secondWord = ["a320", "b747", "Dash 8", "Piper Archer", "Antonov 225", "Cessna 172", "Diamond DA42"]
+    let thirdWord = ["can", "will", "could", "may"]
+    let fourthWord = ["fly", "takeoff", "land", "crash", "taxi"]
+    let fifthWord = ["far", "fast", "slow", "short", "wildly", "calmly"]
 }
